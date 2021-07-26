d404605cd5991c4d7ebdfa39576c1c3deb3f5031
Jchu4 <jerome.chuame@gmail.com>
2021-07-26 21:18:40 +0800

    Factor in for appointments between 8am - 4pm

--- controllers/appointments.mjs
+++ controllers/appointments.mjs
@@ -151,17 +151,32 @@ export default function initAppointmentsController(db) {
     
   const fixAppointmentSave = async (req, res) => {
     const { doctorId, patientId, appTime } = req.body;
+    const appStart = new Date(appTime);
+    const appStartHr = moment(appStart).format('hh:mm:ss a')
+    const appEnd = moment(appStart).add(60, 'm').toDate();
+
+    // To ensure that appointments are between 8am - 4pm
+    const appStartMoment = moment(appStartHr, 'HH:mm:ss');
+    const doctorStart = moment('07:59:59', 'HH:mm:ss');
+    const doctorEnd = moment('15:00:01', 'HH:mm:ss');
     
     try {
-      const create = await db.Appointment.create({
-        doctorId: Number(doctorId),
-        patientId: Number(patientId),
-        startDatetime: new Date(appTime),
-      })
-
-      console.log("CREAT----", create);
-
-      res.render('success-page');
+      // Create appointment only if appTime is available
+      if (appStartMoment.isBetween(doctorStart, doctorEnd)) {
+        const create = await db.Appointment.create({
+                doctorId: Number(doctorId),
+                patientId: Number(patientId),
+                startDatetime: appStart,
+                endDatetime: appEnd,
+              })
+            
+        console.log(create);
+        res.render('success-page');
+      } else {
+        console.log("Failed")
+        // Render failure page
+        // res.render('failure-page');
+      }
     } catch (err) {
       console.log(err);
     }
